1194b3221d4111fb06f3f328917e1e76e4c14bbc
tpmanc <tpxtrime@mail.ru>
2015-09-04 16:21:16 +0300

    add i18n

--- CoreModule.php
+++ CoreModule.php
@@ -2,6 +2,8 @@
 
 namespace tpmanc\cmscore;
 
+use Yii;
+
 class CoreModule extends \yii\base\Module
 {
     public $controllerNamespace = 'tpmanc\cmscore\controllers';
@@ -11,7 +13,30 @@ class CoreModule extends \yii\base\Module
     public function init()
     {
         parent::init();
+        $this->registerTranslations();
+    }
 
-        // custom initialization code goes here
+    private function registerTranslations()
+    {
+        Yii::$app->i18n->translations['core/*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en-US',
+            'basePath' => '@vendor/tpmanc/cms-core/messages',
+            'fileMap' => [
+                'core' => 'core.php',
+                'core/staticPage' => 'coreStaticPage.php',
+                'core/category' => 'coreCategory.php',
+                'core/deliveryType' => 'coreDeliveryType.php',
+                'core/paymentType' => 'corePaymentType.php',
+                'core/product' => 'coreProduct.php',
+                'core/menu' => 'coreMenu.php',
+                'core/user' => 'coreUser.php',
+                'core/productRests' => 'coreProductRests.php',
+                'core/order' => 'coreOrder.php',
+                'core/orderProducts' => 'coreOrderProducts.php',
+                'core/supplier' => 'coreSupplier.php',
+                'core/error' => 'error.php',
+            ],
+        ];
     }
 }
