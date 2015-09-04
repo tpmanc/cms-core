630561c42e566928f940e2fb7064ba446249058d
tpmanc <tpxtrime@mail.ru>
2015-09-04 14:02:08 +0300

    add getVideoCode ,ethod

--- models/Product.php
+++ models/Product.php
@@ -210,6 +210,13 @@ class Product extends \yii\db\ActiveRecord
         parent::afterSave($insert, $changedAttributes);
     }
 
+    public function getVideoCode()
+    {
+        if ($this->video !== '') {
+            return preg_match('|\/embed\/([a-zA-Z0-9\_\-]+)"|is', $this->video, $match)[1];
+        }
+    }
+
     /**
      * Rests relation
      */
